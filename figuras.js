5259c357de0af3fc003d4bec6351e5eaa9c37887
Argenis Rodriguez <estefreric@gmail.com>
2022-01-18 21:06:45 -0400

    Add unid img

--- figuras.js
+++ figuras.js
@@ -48,17 +48,22 @@ function areaCirculo(radio) {
 function calcularPerimetroCuadrado() {
   const input = document.getElementById("InputCuadrado");
   const value = input.value;
+  console.log(value);
+  const und = document.getElementById("unidadCuadrado");
+  console.log(und);
+  const undM = und.value;
 
   const perimetro = perimetroCuadrado(value);
-  //alert(perimetro);
-  document.getElementById("resultadoCuadrado").innerHTML = "Resultado: El perimetro es de " + perimetro + "cm";
+  document.getElementById("resultadoCuadrado").innerHTML = "Resultado: El perimetro es de " + perimetro + " " + undM;
 }
 function calcularAreaCuadrado() {
   const input = document.getElementById("InputCuadrado");
   const value = input.value;
+  const und= document.getElementById("unidadCuadrado");
+  const undM = und.value;
 
   const area = areaCuadrado(value);
-  document.getElementById("resultadoCuadrado").innerHTML = "Resultado: El area es de " + area + "cm";
+  document.getElementById("resultadoCuadrado").innerHTML = "Resultado: El area es de " + area + " " + undM;
 }
 
 //-----------TRIANGULO-------------//
@@ -69,39 +74,49 @@ function calcularPerimetroTriangulo() {
     const value1 = parseInt(input1.value);
     const value2 = parseInt(input2.value);
     const value3 = parseInt(input3.value);
+    const und= document.getElementById("unidadTriangulo");
+    const undM = und.value;
 
     const perimetro = perimetroTriangulo(value1, value2, value3);
-    document.getElementById("resultadoTriangulo").innerHTML = "Resultado: El perimetro es de " + perimetro + "cm";
+    document.getElementById("resultadoTriangulo").innerHTML = "Resultado: El perimetro es de " + perimetro + " " + undM;
   }
   function calcularAreaTriangulo() {
     const input1 = document.getElementById("InputTriangulo2");
     const input2 = document.getElementById("InputTriangulo3");
     const value1 = parseInt(input1.value);
     const value2 = parseInt(input2.value);
+    const und= document.getElementById("unidadTriangulo");
+    const undM = und.value;
 
     const area = areaTriangulo(value1, value2);
-    document.getElementById("resultadoTriangulo").innerHTML = "Resultado: El area es de " + area + "cm";
+    document.getElementById("resultadoTriangulo").innerHTML = "Resultado: El area es de " + area + " " + undM;
   }
 
   //-----------CIRCULO-------------//
 function calcularDiametroCirculo() {
     const input1 = document.getElementById("InputCirculo");
     const value1 = parseInt(input1.value);
+    const und= document.getElementById("unidadCirculo");
+    const undM = und.value;
 
     const diametro = diametroCirculo(value1);
-    document.getElementById("resultadoCirculo").innerHTML = "Resultado: El diametro es de " + diametro + "cm";
+    document.getElementById("resultadoCirculo").innerHTML = "Resultado: El diametro es de " + diametro + " " + undM;
   }
   function calcularPerimetroCirculo() {
     const input1 = document.getElementById("InputCirculo");
     const value1 = parseInt(input1.value);
+    const und= document.getElementById("unidadCirculo");
+    const undM = und.value;
 
     const perimetro = perimetroCirculo(value1);
-    document.getElementById("resultadoCirculo").innerHTML = "Resultado: La circunferencia es de " + perimetro + "cm";
+    document.getElementById("resultadoCirculo").innerHTML = "Resultado: La circunferencia es de " + perimetro + " " + undM;
   }
   function calcularAreaCirculo() {
     const input1 = document.getElementById("InputCirculo");
     const value1 = parseInt(input1.value);
+    const und= document.getElementById("unidadCirculo");
+    const undM = und.value;
 
     const area = areaCirculo(value1);
-    document.getElementById("resultadoCirculo").innerHTML = "Resultado: El area es de " + area + "cm";
+    document.getElementById("resultadoCirculo").innerHTML = "Resultado: El area es de " + area + " " + undM;
   }
